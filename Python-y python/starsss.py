891a4041442a9266da777792a103ed35f23d70ac
Lafayette-Marquis <90972245+Lafayette-Marquis@users.noreply.github.com>
2024-08-15 13:24:13 -0400

    edited Starsss.py to add box around the stars

--- Python-y python/starsss.py
+++ Python-y python/starsss.py
@@ -1,6 +1,7 @@
 import turtle
 
 alexa = turtle.Turtle()
+alexa.speed(0)
 alexa.penup()
 alexa.backward(200) 
 
@@ -12,6 +13,20 @@ for i in range(5):
   alexa.penup()
   alexa.forward(350)
   alexa.right(144)
-
+alexa.pendown()
+alexa.left(90)
+alexa.forward(150)
+alexa.right(90)
+alexa.forward(350)
+alexa.right(90)
+alexa.forward(360)
+alexa.right(90)
+alexa.forward(350)
+alexa.right(90)
+alexa.forward(215)
+#for i in range(4):
+#  alexa.pendown()
+#  alexa.forward(350)
+#  alexa.left(90)
 wn = turtle.Screen()
 wn.exitonclick()
