eba54a1b6628e213ec679bcdc22239b49286e7bc
Lafayette-Marquis <90972245+Lafayette-Marquis@users.noreply.github.com>
2024-09-05 13:09:48 -0400

    Added variables to hello.f90

--- JSON-YAML-JS-C/hello.f90
+++ JSON-YAML-JS-C/hello.f90
@@ -1,4 +1,17 @@
 program hello
   ! This is a comment line; it is ignored by the compiler
   print *, 'Hello, World!'
+  implicit none
+
+  integer :: amount
+  real :: pi
+  complex :: frequency
+  character :: initial
+  logical :: isOkay
+  amount = 10
+  pi = 3.1415927
+  frequency = (1.0, -0.5)
+  initial = 'A'
+  isOkay = .false.
+  print *, amount, pi, frequency, initial, isOkay
 end program hello
