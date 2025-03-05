0fac88c5e1153294286c9e1315647aeb5021998b
Lafayette-Marquis <90972245+Lafayette-Marquis@users.noreply.github.com>
2025-03-04 20:29:54 -0500

    Somehow broke the crew list...

--- HTMLHTML/somethinghotel/script.js
+++ HTMLHTML/somethinghotel/script.js
@@ -1,9 +1,9 @@
-
 let text = '{ "employees" : [' +
 '{ "firstName":"Lexi" , "lastName":"Rejunski", "jobtitle":"Host"},' +
 '{ "firstName":"Charlotte" , "lastName":"Smith", "jobtitle":"Barista"},' +
 '{ "firstName":"Peter" , "lastName":"Parker", "jobtitle":"Housekeeper manager"},' + 
-'{ "firstName":"Javier" , "lastName":"Javier", "jobtitle":"Housekeeper"} ]}';
+'{ "firstName":"Javier" , "lastName":"Javier", "jobtitle":"Housekeeper"}' +
+'{ "firstName":"Malachi", "lastName":"Anderson", "jobtitle":"Kitchen Staff Manager"} ]}';
        const obj = JSON.parse(text);/*list of the employees to be typed*/
 
 const toggleButton = document.getElementById('toggle-button');
@@ -24,4 +24,5 @@ document.getElementById("employees").innerHTML =
         obj.employees[0].firstName + " " + obj.employees[0].lastName + ", who is the " + obj.employees[0].jobtitle + ", " +
         obj.employees[1].firstName + " " + obj.employees[1].lastName + ", who is the " + obj.employees[1].jobtitle + ", " +
         obj.employees[2].firstName + " " + obj.employees[2].lastName + ", who is the " + obj.employees[2].jobtitle + ", " +
-        obj.employees[3].firstName + " " + obj.employees[3].lastName + ", who is the " + obj.employees[3].jobtitle + ".";
+        obj.employees[3].firstName + " " + obj.employees[3].lastName + ", who is the " + obj.employees[3].jobtitle + ", " +
+        obj.employees[4].firstName + " " + obj.employees[4].lastName + ", who is the " + obj.employees[4].jobtitle + ".";
