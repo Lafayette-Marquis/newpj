708b2863843c9bbe689f07157da265d740d2f7f6
Lafayette-Marquis <90972245+Lafayette-Marquis@users.noreply.github.com>
2025-05-29 14:02:10 -0400

    Edited the comments at the top of Alarm.py

--- Python-y python/Alarm.py
+++ Python-y python/Alarm.py
@@ -1,6 +1,6 @@
 #this is the time file
-# Many people keep time using a 24 hour clock (11 is 11am and 23 is 11pm, 0 is midnight). 
-#If it is currently 13 and you set your alarm to go off in 50 hours, it will be 15 (3pm). 
+#Many people keep time using a 24 hour clock (1100 is 11:00 am and 2300 is 11:00 pm, 0000 is midnight). 
+#If it is currently 13 and you set your alarm to go off in 50 hours, it will be 1500 (3pm). 
 #Write a Python program to solve the general version of the above problem. 
 #Ask the user for the time now (in hours), and then ask for the number of hours to wait for the alarm. 
 #Your program should output what the time will be on the clock when the alarm goes off.
