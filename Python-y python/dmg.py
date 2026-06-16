cd5b4e29d8cc35ee098d102714c118f4dd424133
Lafayette-Marquis <90972245+Lafayette-Marquis@users.noreply.github.com>
2026-06-15 22:10:01 -0400

    changed random.randint to between 1 and 1.125 instead of between 1 and 1,125.

--- Python-y python/dmg.py
+++ Python-y python/dmg.py
@@ -4,6 +4,6 @@ Power = int(input("enter your strength "))
 magickpower = int(input("enter your magick power "))
 lv = int(input("enter your level "))
 
-dmg = ((Power * random.randint(1, 1125))/100 - magickpower) * (2 + magickpower * (lv + magickpower))
+dmg = ((Power * random.randint(1, 1.125))/100 - magickpower) * (2 + magickpower * (lv + magickpower))
 
 print("The average amount of damage you with hit with is ", dmg)
