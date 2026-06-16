14c30549946f75bd2fbaec0c9ffee337ab8a392c
Lafayette-Marquis <90972245+Lafayette-Marquis@users.noreply.github.com>
2026-06-15 22:01:18 -0400

    Added explanatory comment to the beginning of Detect.py

--- Python-y python/Detect.py
+++ Python-y python/Detect.py
@@ -1,4 +1,4 @@
-r = input()
+r = input() # IGNORE --- Detects if the input is a string, and if it is not, it will print the type of the input and exit the program. If it is a string, it will print the input and its type.
 r.strip()
 for inp in r:
     result = (inp)
